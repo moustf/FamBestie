f021b9f16cfa4436a30363553e04525adbe26970
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-03 22:49:21 +0200

    Created the database models and the relations between them close #6

---
+++ server/models/users.ts
@@ -0,0 +1,44 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database/connection';
+
+export const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isIn: {
+        args: [['admin', 'client', 'worker']],
+        msg: 'Must be either admin, client, or worker',
+      },
+    },
+  },
+  visibility: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+});
