a9553ea744ac0be1b3c2d4594824ad050e09e1f0
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-05 23:52:02 +0200

    Edited the jwt functions return type close #36

--- server/utils/jwt/index.ts
+++ server/utils/jwt/index.ts
@@ -5,7 +5,7 @@ import { TokenPayload } from '../interfaces/tokenPayload';
 
 const { secretKey } = baseConfig;
 
-export const signToken = (payload: TokenPayload) => new Promise((resolve, reject) => {
+export const signToken = (payload: TokenPayload) => new Promise<ReturnType<any>>((resolve, reject) => {
   sign(payload, secretKey as Secret, (err, token) => {
     if (err) {
       reject(err);
@@ -15,7 +15,7 @@ export const signToken = (payload: TokenPayload) => new Promise((resolve, reject
   });
 });
 
-export const verifyToken = (token: string) => new Promise((resolve, reject) => {
+export const verifyToken = (token: string) => new Promise<ReturnType<any>>((resolve, reject) => {
   verify(token, secretKey as Secret, (err, decoded) => {
     if (err) {
       reject(err);
