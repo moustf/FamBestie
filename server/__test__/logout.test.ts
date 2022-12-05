02e0403478c4b5258c192034b953b354fb496dd5
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-05 09:58:26 +0200

    Created the logout route Tested if the route deletes the token header close #9

---
+++ server/__test__/logout.test.ts
@@ -0,0 +1,25 @@
+/* eslint-disable max-len */
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+import supertest from 'supertest';
+
+import { app } from '../app';
+import { buildSeed } from '../database/seed';
+
+beforeAll(() => buildSeed());
+
+describe('Testing logout route', () => {
+  it('Should parse the token from the browser when sent with the request.', (done) => {
+    supertest(app)
+      .post('/api/v1/auth/logout')
+      .set('Cookie', [
+        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic3R1ZGVudCIsIm5hbWUiOiJKb2huIERvZSIsImlkIjoxNTE2MjM5MDIyfQ.ivV7KczMBPLI6JBiY7oAXlcfPuaTVNtd71aTrtgZa8A',
+      ])
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.header).not.toHaveProperty('token');
+        return done();
+      });
+  });
+});
