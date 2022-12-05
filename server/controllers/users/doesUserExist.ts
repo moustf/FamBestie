c489749d1549975267c705843cfe1b266faa1a4a
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-05 20:19:21 +0200

    Created the conroller for the rute Tested the edge cases of the route Served the route close #14

---
+++ server/controllers/users/doesUserExist.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { doesUserExist } from '../../queries/auth/doesUserExist';
+import { CustomError } from '../../utils/custom_error';
+
+export const doesEmailExistController = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  try {
+    const { email } = req.params;
+
+    const doesEmailExist = await doesUserExist(email);
+
+    if (doesEmailExist?.getDataValue('id')) {
+      throw new CustomError(302, 'The user does already exist!');
+    }
+
+    return res.status(202).json({ msg: 'The user does not exist, you can create an account!' });
+  } catch (error) {
+    next(error);
+  }
+};
