3f556e3a0e4895fe809d64e84c77e78d9b1a905d
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-06 15:38:47 +0200

    Created the register worker route Tested the route Created the interface for the request body and the validation Served the route on the  router close#35

---
+++ server/controllers/worker/registerWorker.ts
@@ -0,0 +1,51 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { registerWorkerQuery } from '../../queries/worker/registerWorkerQuery';
+import { doesWorkerExistQuery } from '../../queries/worker/doesWorkerExist';
+import { registerWorkerSchema } from '../../utils/validation/admin/registerWorkerValidation';
+import { CustomError } from '../../utils/custom_error';
+
+export const registerWorkerController = async (req: Request, res: Response, next: NextFunction): Promise<ReturnType<any>> => {
+  try {
+    const {
+      name,
+      email,
+      phone,
+      location,
+      gender,
+      specialty,
+      dateOfBirth,
+      yearsOfExperience,
+    } = await registerWorkerSchema.validate(req.body);
+
+    const doesWorkerExist = await doesWorkerExistQuery(email);
+
+    if (doesWorkerExist?.getDataValue('id')) {
+      throw new CustomError(403, 'The user is already exists!');
+    }
+
+    const worker = await registerWorkerQuery({
+      name,
+      email,
+      phone,
+      location,
+      gender,
+      specialty,
+      date_of_birth: dateOfBirth,
+      years_of_experience: yearsOfExperience,
+      state: 'unemployed',
+      hiring_date: null,
+    });
+
+    if (!worker.getDataValue('id')) {
+      throw new CustomError(400, 'The worker has entered an invalid data!');
+    }
+
+    return res.status(201).json({ msg: 'The worker has registered successfully!', data: worker });
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      next(new CustomError(400, 'The worker has entered an invalid data!'));
+    }
+    next(error);
+  }
+};
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 import { authRouter } from './auth';
 import { usersRouter } from './users';
+import { workerRouter } from './worker';
 
 export const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/user', usersRouter);
+router.use('/worker', workerRouter);
