cef7242d7e06925553a0159a29ef735da7ff1cbc
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-07 16:31:12 +0200

    Created getJobsByCategory route and the query to get the data from the database Tested the route and serverd the route in the server close #46

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -7,6 +7,7 @@ import { getAllJobsController } from '../controllers/admin/getAllJobsController'
 import { getAllClientsController } from '../controllers/admin/getAllClientsController';
 import { getAllWorkersController } from '../controllers/admin/getAllWorkersController';
 import { getAllClientsForWorkerController } from '../controllers/admin/getAllClientsForWorker';
+import { getJobsByCategoryController } from '../controllers/admin/getJobsByCategoryController';
 import { userAuth } from '../middlewares/userAuth';
 import { roleAuth } from '../middlewares/roleAuth';
 
@@ -19,3 +20,4 @@ adminRouter.get('/jobs', userAuth, roleAuth('admin'), getAllJobsController);
 adminRouter.get('/clients', userAuth, roleAuth('admin'), getAllClientsController);
 adminRouter.get('/workers', userAuth, roleAuth('admin'), getAllWorkersController);
 adminRouter.get('/worker/:id/clients', userAuth, roleAuth('admin'), getAllClientsForWorkerController);
+adminRouter.get('/jobs/:category', userAuth, roleAuth('admin'), getJobsByCategoryController);
