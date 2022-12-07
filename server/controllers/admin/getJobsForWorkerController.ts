eb9f2d3d6801fd6b3b8c86abff22b4f0a4b27e88
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-07 13:51:42 +0200

    Pulled the main, removed the elsint turn off commit in the route test file, and removed the return type of the controller close #42

--- server/controllers/admin/getJobsForWorkerController.ts
+++ server/controllers/admin/getJobsForWorkerController.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 import { getJobsForWorkerQuery } from '../../queries/admin/getJobsForWorkerQuery';
 import { CustomError } from '../../utils/custom_error';
 
-export const getJobsForWorkerController = async (req: Request, res: Response, next: NextFunction): Promise<ReturnType<any>> => {
+export const getJobsForWorkerController = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
 
