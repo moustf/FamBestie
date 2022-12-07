5f179ec2120fb5c251efa14161e80c478924cf7d
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-07 13:02:49 +0200

    Resolved conflicts close #39

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import { getAllClientsController } from '../controllers/admin/getAllClientsController';
+import { getAllClientsForWorkerController } from '../controllers/admin/getAllClientsForWorker';
 import { userAuth } from '../middlewares/userAuth';
 import { roleAuth } from '../middlewares/roleAuth';
 
 export const adminRouter = Router();
 
 adminRouter.get('/clients', userAuth, roleAuth('admin'), getAllClientsController);
+adminRouter.get('/worker/:id/clients', userAuth, roleAuth('admin'), getAllClientsForWorkerController);
