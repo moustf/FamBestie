24045f5b42165f85f49fd1e04cd6e53e9d8ec78b
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-12 20:42:10 +0200

    Added the color palette for the website and wrapped the app inside it close #65

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,38 @@
 import { FC } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './App.css';
 
 import { Login } from './pages/Login';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757de8',
+      main: '#3f51b5',
+      dark: '#002984',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000000',
+    },
+  },
+});
+
 const App: FC = () => {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <Login />
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Login />
+        </div>
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
