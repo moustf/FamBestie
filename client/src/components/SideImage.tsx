db08a17f72371858432d6b94ad38b307c1bc1601
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-12 23:50:23 +0200

    Separated the styles into another file, and removed the important statements to use inline styles overrides close #65

--- client/src/components/SideImage.tsx
+++ client/src/components/SideImage.tsx
@@ -1,37 +1,21 @@
 import { FC } from 'react';
 import { ImageListItem } from '@mui/material';
+import { styles } from './styles';
 
 export const SideImage: FC = () => {
   const imageURL = 'https://images.pexels.com/photos/9462315/pexels-photo-9462315.jpeg?auto=compress&cs=tinysrgb&w=1600';
 
   return (
     <ImageListItem
-      sx={{
-        width: '40% !important',
-        height: '100vh !important',
-        position: 'relative',
-        boxShadow: '1px 1px 5px 0px rgba(0, 0, 0, 0.25)',
-        borderTopLeftRadius: '1.5rem',
-        borderBottomLeftRadius: '1.5rem',
-        overflow: 'hidden',
-      }}
+      sx={styles.imageItem}
+      style={{ width: '38%', height: '100vh' }}
     >
-      <div style={{
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        right: '0',
-        top: '0',
-        backgroundColor: 'rgba(0, 0, 0, 0.15)',
-      }}
-      />
+      <div style={styles.overlay} />
       <img
         src={`${imageURL}`}
         alt="Side cloth closet"
         loading="lazy"
-        style={{
-          objectFit: 'fill',
-        }}
+        style={styles.image}
       />
     </ImageListItem>
   );
