fe55947ef9258a089679cd10afd2792c73b48ccf
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-15 01:07:39 +0200

    Created part of the signup page with its functionality and stepper component to be continued close #66

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,39 +1,37 @@
-import { FC } from 'react';
 import axios from 'axios';
+import { FC, useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import {
-  Container, CssBaseline, Box, Typography, Avatar, Button,
+  Container,
+  CssBaseline,
+  Box,
+  Typography,
+  Avatar,
 } from '@mui/material';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import { useForm, SubmitHandler, FieldValue } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import {
+  useForm, SubmitHandler, FieldValue,
+} from 'react-hook-form';
+// import { yupResolver } from '@hookform/resolvers/yup';
 
+import { CustomStepper } from '../components/Stepper';
 import { InputFiled } from '../components/InputField';
-import { signupSchema } from '../utils/validation/signupData';
+// import { signupSchema } from '../utils/validation/signupData';
 import { SideImage } from '../components/SideImage';
 import { styles } from './formsStyles';
 
 export const Signup: FC = () => {
+  const [activeStep, setActiveStep] = useState(0);
+
   useMutation({
-    mutationKey: ['signup'],
     mutationFn: (inputs) => (
       axios.post('/auth/signup', inputs)
     ),
   });
 
-  const { control, handleSubmit } = useForm({
-    defaultValues: {
-      name: '',
-      email: '',
-      password: '',
-      passwordConfirmation: '',
-      location: '',
-      phoneNumber: '',
-    },
-    resolver: yupResolver(signupSchema),
-  });
+  const { control, handleSubmit } = useForm();
 
-  const onSubmit: SubmitHandler<FieldValue<any>> = () => console.log('here');
+  const onSignupSubmit: SubmitHandler<FieldValue<any>> = (data: any) => console.log(data);
 
   return (
     <Container
@@ -55,13 +53,32 @@ export const Signup: FC = () => {
         <Container
           component="form"
           maxWidth={false}
-          onSubmit={handleSubmit(onSubmit)}
+          onSubmit={handleSubmit(onSignupSubmit)}
+          sx={styles.formContainer}
         >
           <InputFiled
             control={control}
             fieldName="name"
-            type="string"
-            placeholder="Type your name ..."
+            type="text"
+            placeholder="Please enter your name ..."
+          />
+          <InputFiled
+            control={control}
+            fieldName="email"
+            type="email"
+            placeholder="Please enter your email ..."
+          />
+          <InputFiled
+            control={control}
+            fieldName="location"
+            type="text"
+            placeholder="Please enter your location ..."
+          />
+          {/* <InputFiled
+            control={control}
+            fieldName="password confirmation"
+            type="password"
+            placeholder="Please confirm your password ..."
           />
           <Button
             type="submit"
@@ -70,8 +87,12 @@ export const Signup: FC = () => {
             sx={styles.submitButton}
           >
             Sign Up
-          </Button>
+          </Button> */}
         </Container>
+        <CustomStepper
+          activeStep={activeStep}
+          setActiveStep={setActiveStep}
+        />
       </Box>
       <SideImage />
     </Container>
