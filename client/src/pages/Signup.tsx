a95e637e02b25ac8d2cf295030ec669903ab6821
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-15 13:04:35 +0200

    Finshed the stepper component logic to show the inputs according to their position Added the error messages in case validation errors and added a validation to test if all the inputs are filled or not And finshed the signup form close #66

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -7,31 +7,53 @@ import {
   Box,
   Typography,
   Avatar,
+  Button,
+  Link,
+  Alert,
 } from '@mui/material';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import {
   useForm, SubmitHandler, FieldValue,
 } from 'react-hook-form';
-// import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import { CustomStepper } from '../components/Stepper';
 import { InputFiled } from '../components/InputField';
-// import { signupSchema } from '../utils/validation/signupData';
+import { signupSchema } from '../utils/validation/signupData';
 import { SideImage } from '../components/SideImage';
 import { styles } from './formsStyles';
 
 export const Signup: FC = () => {
   const [activeStep, setActiveStep] = useState(0);
 
-  useMutation({
+  const { mutate } = useMutation({
     mutationFn: (inputs) => (
       axios.post('/auth/signup', inputs)
     ),
   });
 
-  const { control, handleSubmit } = useForm();
+  const { control, handleSubmit, formState: { errors, dirtyFields } } = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      location: '',
+      phone: '',
+    },
+    resolver: yupResolver(signupSchema),
+  });
 
-  const onSignupSubmit: SubmitHandler<FieldValue<any>> = (data: any) => console.log(data);
+  const isValid = Boolean(
+    dirtyFields.name
+      && dirtyFields.email
+      && dirtyFields.location
+      && dirtyFields.password
+      && dirtyFields.passwordConfirmation
+      && dirtyFields.phone,
+  );
+
+  const onSignupSubmit: SubmitHandler<FieldValue<any>> = (data: any) => mutate(data);
 
   return (
     <Container
@@ -61,38 +83,116 @@ export const Signup: FC = () => {
             fieldName="name"
             type="text"
             placeholder="Please enter your name ..."
+            activeStep={activeStep}
           />
           <InputFiled
             control={control}
             fieldName="email"
             type="email"
             placeholder="Please enter your email ..."
+            activeStep={activeStep}
           />
           <InputFiled
             control={control}
             fieldName="location"
             type="text"
             placeholder="Please enter your location ..."
+            activeStep={activeStep}
           />
-          {/* <InputFiled
+          <InputFiled
             control={control}
-            fieldName="password confirmation"
+            fieldName="password"
+            type="password"
+            placeholder="Please enter your password ..."
+            activeStep={activeStep}
+          />
+          <InputFiled
+            control={control}
+            fieldName="passwordConfirmation"
             type="password"
             placeholder="Please confirm your password ..."
+            activeStep={activeStep}
           />
-          <Button
-            type="submit"
-            variant="contained"
-            fullWidth
-            sx={styles.submitButton}
-          >
-            Sign Up
-          </Button> */}
+          <InputFiled
+            control={control}
+            fieldName="phone"
+            type="number"
+            placeholder="Please enter your phone number ..."
+            activeStep={activeStep}
+          />
+          {
+            errors.name && activeStep === 0
+              && <Alert variant="standard" severity="error">{errors.name.message}</Alert>
+          }
+          {
+            errors.email && activeStep === 0
+              && <Alert variant="standard" severity="error">{errors.email.message}</Alert>
+          }
+          {
+            errors.location && activeStep === 0
+              && <Alert variant="standard" severity="error">{errors.location.message}</Alert>
+          }
+          {
+            errors.password && activeStep === 1
+              && <Alert variant="standard" severity="error">{errors.password.message}</Alert>
+          }
+          {
+            errors.passwordConfirmation && activeStep === 1
+              && <Alert variant="standard" severity="error">{errors.passwordConfirmation.message}</Alert>
+          }
+          {
+            errors.phone && activeStep === 2
+              && <Alert variant="standard" severity="error">{errors.phone.message}</Alert>
+          }
+          {activeStep === 2 && (
+            <Button
+              type="submit"
+              variant="contained"
+              fullWidth
+              sx={styles.submitButton}
+              disabled={!isValid}
+            >
+              Sign Up
+            </Button>
+          )}
         </Container>
         <CustomStepper
           activeStep={activeStep}
           setActiveStep={setActiveStep}
         />
+        <Typography
+          variant="body1"
+          color="text.secondary"
+          align="center"
+          sx={styles.signupFirstTypography}
+        >
+          You already have an account?
+          {' '}
+          <Button variant="text">
+            <Link color="inherit" href="/login">Sign In</Link>
+          </Button>
+          Now.
+        </Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          align="center"
+          sx={styles.signupSecondTypography}
+        >
+          All Rights Reserver Copyright ©
+          {' '}
+          <Link
+            color="inherit"
+            href="https://github.com/moustf"
+            target="_blank"
+          >
+            Mustafa Salem
+          </Link>
+          {' '}
+          {new Date().getFullYear()}
+          {' '}
+          .
+        </Typography>
       </Box>
       <SideImage />
     </Container>
