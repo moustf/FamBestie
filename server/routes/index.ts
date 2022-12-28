1db00a50df4541118b25efb8038ee63920dd8947
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-28 17:20:26 +0200

    Moved the auth controllers to index file Served the new statistics route close #89

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -6,10 +6,13 @@ import { adminRouter } from './admin';
 import { workerRouter } from './worker';
 import { clientRouter } from './client';
 
+import { userAuth } from '../middlewares/userAuth';
+import { roleAuth } from '../middlewares/roleAuth';
+
 export const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/user', usersRouter);
 router.use('/worker', workerRouter);
-router.use('/admin', adminRouter);
-router.use('/client', clientRouter);
+router.use('/admin', userAuth, roleAuth('admin'), adminRouter);
+router.use('/client', userAuth, roleAuth('client'), clientRouter);
