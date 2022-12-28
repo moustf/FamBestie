0be1332fa0554e91d53884f0ff1f75d98203145b
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-28 13:33:54 +0200

    Edited the getWorkerById query to retur all the reviews and some other job details for the worker and also edited the tests to include testing for user_workers array close #88

--- server/queries/admin/getWorkerByIdQuery.ts
+++ server/queries/admin/getWorkerByIdQuery.ts
@@ -1,7 +1,22 @@
-import { Worker } from '../../models/workers';
+import { models } from '../../models';
+
+const { Worker, UserWorker, Job } = models;
 
 export const getWorkerByIdQuery = (id: number) => Worker.findOne({
   where: {
     id,
   },
+  include: [{
+    model: UserWorker, // ? Array of job review and other data objects in case of more than one job.
+    where: {
+      worker_id: id,
+    },
+    attributes: {
+      exclude: ['createdAt', 'updatedAt', 'user_id', 'worker_id', 'job_id'],
+    },
+    include: [{
+      model: Job,
+      attributes: ['id', 'stars', 'review_text', 'working_hours', 'bill'],
+    }],
+  }],
 });
