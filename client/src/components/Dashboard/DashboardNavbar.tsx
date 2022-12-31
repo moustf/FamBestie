4183d2c2dbc07808d5b72c0d5b654f0920e1df64
Mustafa Salem <mustafaissasalem@gmail.com>
2022-12-31 13:33:41 +0200

    Finshed the dahsboard navbar component close #97

--- client/src/components/Dashboard/DashboardNavbar.tsx
+++ client/src/components/Dashboard/DashboardNavbar.tsx
@@ -1,6 +1,8 @@
-import { FC } from 'react';
-import { Link } from 'react-router-dom';
-import { Box, ListItem, Button } from '@mui/material';
+import { FC, useState, SyntheticEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Box, Tabs, Tab, Typography,
+} from '@mui/material';
 import {
   Insights as InsightsIcon,
   People as PeopleIcon,
@@ -11,75 +13,103 @@ import {
 const items = [
   {
     href: '/dashboard/statistics',
-    icon: <InsightsIcon sx={{ fontSize: 'small' }} />,
+    icon: <InsightsIcon sx={{ fontSize: '2rem' }} />,
     title: 'Statistics',
   },
   {
     href: '/dashboard/clients',
-    icon: <PeopleIcon sx={{ fontSize: 'small' }} />,
+    icon: <PeopleIcon sx={{ fontSize: '2rem' }} />,
     title: 'Clients',
   },
   {
     href: '/dashboard/workers',
-    icon: <HailIcon sx={{ fontSize: 'small' }} />,
+    icon: <HailIcon sx={{ fontSize: '2rem' }} />,
     title: 'Workers',
   },
   {
     href: '/dashboard/jobs',
-    icon: <WorkIcon sx={{ fontSize: 'small' }} />,
+    icon: <WorkIcon sx={{ fontSize: '2rem' }} />,
     title: 'Jobs',
   },
 ];
 
-export const DashboardNavbar: FC = () => (
-  <Box sx={{
-    width: '100%',
-    height: '100%',
-    bgcolor: 'primary.light',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }}
-  >
-    {
-      items.map((item) => (
-        <ListItem
-          disableGutters
-          sx={{
-            display: 'flex',
-            mb: 0.5,
-            py: 0,
-            px: 2,
-          }}
-          title={item.title}
-        >
-          <Link to={item.href}>
-            <Button
-              startIcon={item.icon}
-              disableRipple
-              sx={{
-                borderRadius: 1,
-                color: 'secondary.dark',
-                fontWeight: 'fontWeightBold',
-                justifyContent: 'flex-start',
-                px: 3,
-                textAlign: 'left',
-                textTransform: 'none',
-                width: '100%',
-                '& .MuiButton-startIcon': {
-                  color: 'secondary.main',
-                },
-                '&:hover': {
-                  backgroundColor: 'rgba(255,255,255, 0.08)',
-                },
-              }}
-            >
-              {item.title}
-            </Button>
-          </Link>
-        </ListItem>
-      ))
-    }
-  </Box>
-);
+export const DashboardNavbar: FC = () => {
+  const [value, setValue] = useState(0);
+  const navigate = useNavigate();
+
+  const handleChange = (event: SyntheticEvent, newValue: number): void => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{
+      width: '100%',
+      height: '100%',
+      padding: 5,
+      bgcolor: 'primary.light',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      gap: '15rem',
+    }}
+    >
+      <Typography
+        component="h1"
+        variant="h4"
+        sx={{
+          color: 'primary.dark',
+          textShadow: '0px 0px 5px rgba(255, 255, 255, 0.5)',
+          position: 'relative',
+          transform: 'skew(-20deg)',
+          letterSpacing: '2px',
+          '&::after': {
+            content: '" "',
+            width: '100%',
+            height: '0.3rem',
+            borderRadius: '1rem',
+            position: 'absolute',
+            left: '0',
+            bottom: '-1rem',
+            zIndex: '10',
+            backgroundColor: 'primary.dark',
+            boxShadow: '0px 0px 5px 0px rgba(255, 255, 255, 0.5)',
+          },
+        }}
+      >
+        Admin Dashboard
+      </Typography>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        centered
+        orientation="vertical"
+        sx={{
+          width: '50%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '1rem',
+        }}
+      >
+        {
+            items.map((item) => (
+              <Tab
+                icon={item.icon}
+                iconPosition="end"
+                label={item.title}
+                sx={{
+                  width: '80%',
+                  color: 'primary.dark',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                }}
+                onClick={() => navigate(item.href)}
+              />
+            ))
+          }
+      </Tabs>
+    </Box>
+  );
+};
