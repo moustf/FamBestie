498640ab278e0350b263f53f621ee7838fab9385
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-02 20:49:33 +0200

    Edited the attributes returned from the all jobs query and added a condition to return all the jobs as a default option for the component close #101

--- server/queries/admin/getAllJobsQuery.ts
+++ server/queries/admin/getAllJobsQuery.ts
@@ -5,10 +5,15 @@ const { Worker, UserWorker, Job } = models;
 // ? The query will perform a left join to get always the Jobs, and if the job have been filled, the
 // ? query will return the worker's info.
 export const getAllJobsQuery = () => Job.findAll({
+  attributes: {
+    exclude: ['field', 'createdAt', 'updatedAt'],
+  },
   include: [{
     model: UserWorker,
+    attributes: ['id'],
     include: [{
       model: Worker,
+      attributes: ['id', 'name', 'email', 'phone', 'specialty'],
     }],
   }],
 });
--- server/queries/admin/getJobsByCategoryQuery.ts
+++ server/queries/admin/getJobsByCategoryQuery.ts
@@ -1,7 +1,19 @@
 import { Job } from '../../models/jobs';
 
-export const getJobsByCategoryQuery = (category: string) => Job.findAll({
-  where: {
-    title: category,
-  },
-});
+export const getJobsByCategoryQuery = (category: string) => {
+  if (category === 'all') {
+    return Job.findAll({
+      attributes: {
+        exclude: ['field', 'createdAt', 'updatedAt'],
+      },
+    });
+  }
+  return Job.findAll({
+    attributes: {
+      exclude: ['field', 'createdAt', 'updatedAt'],
+    },
+    where: {
+      field: category,
+    },
+  });
+};
