ee9074325aaa695c82e1b367fecd4149776b4418
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-02 13:37:43 +0200

    Edited the clients query to not return the admin info and to customize the returned attributes, also deleted the condition from the getClientById query to perform a outer left join close #100

--- server/queries/admin/getAllClientsQuery.ts
+++ server/queries/admin/getAllClientsQuery.ts
@@ -1,3 +1,13 @@
+import { Op } from 'sequelize';
 import { User } from '../../models/users';
 
-export const getAllClientsQuery = () => User.findAll();
+export const getAllClientsQuery = () => User.findAll({
+  attributes: {
+    exclude: ['password', 'updatedAt', 'role'],
+  },
+  where: {
+    id: {
+      [Op.ne]: 1,
+    },
+  },
+});
