2b9689c02e70cb402364674d490870a9f0e8d88b
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-03 23:57:59 +0200

    Deleted the workers review route for the client and made teh workers info route just workers to return the personal info and the review info, and edited the query to return just the needed data from the backend close #109

--- server/__test__/getWorkersList.test.ts
+++ server/__test__/getWorkersList.test.ts
@@ -9,7 +9,7 @@ beforeAll(() => buildSeed());
 describe('Testing getWorkersList route for clients', () => {
   it('In the success path, it should return 200 status code.', (done) => {
     supertest(app)
-      .get('/api/v1/client/workers/info')
+      .get('/api/v1/client/workers')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwibmFtZSI6IkthbWFsIEhhc3NhbiIsInJvbGUiOiJjbGllbnQiLCJpYXQiOjE2NzA0MzA1MjV9.g43RwJczntSutdM9B90V9ga1ujcl3ck0U28olNQwE40',
       ])
@@ -22,7 +22,7 @@ describe('Testing getWorkersList route for clients', () => {
 
   it('In the success path, it should return 200 status code and the success message.', (done) => {
     supertest(app)
-      .get('/api/v1/client/workers/info')
+      .get('/api/v1/client/workers')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwibmFtZSI6IkthbWFsIEhhc3NhbiIsInJvbGUiOiJjbGllbnQiLCJpYXQiOjE2NzA0MzA1MjV9.g43RwJczntSutdM9B90V9ga1ujcl3ck0U28olNQwE40',
       ])
@@ -36,7 +36,7 @@ describe('Testing getWorkersList route for clients', () => {
 
   it('In the success path, it should return 200 status code and the data array in the body.', (done) => {
     supertest(app)
-      .get('/api/v1/client/workers/info')
+      .get('/api/v1/client/workers')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwibmFtZSI6IkthbWFsIEhhc3NhbiIsInJvbGUiOiJjbGllbnQiLCJpYXQiOjE2NzA0MzA1MjV9.g43RwJczntSutdM9B90V9ga1ujcl3ck0U28olNQwE40',
       ])
@@ -50,7 +50,7 @@ describe('Testing getWorkersList route for clients', () => {
 
   it('In the success path, it should return 200 status code and the first object in the data array in the body.', (done) => {
     supertest(app)
-      .get('/api/v1/client/workers/info')
+      .get('/api/v1/client/workers')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwibmFtZSI6IkthbWFsIEhhc3NhbiIsInJvbGUiOiJjbGllbnQiLCJpYXQiOjE2NzA0MzA1MjV9.g43RwJczntSutdM9B90V9ga1ujcl3ck0U28olNQwE40',
       ])
@@ -64,7 +64,7 @@ describe('Testing getWorkersList route for clients', () => {
 
   it('In the failure path, it should return 404 status code and not found msg.', (done) => {
     supertest(app)
-      .get('/api/v1/client/workers/info')
+      .get('/api/v1/client/workers')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTAsIm5hbWUiOiJub09uZSIsInJvbGUiOiJjbGllbnQiLCJpYXQiOjE2NzA0MjYyMzN9.CiFz0Mu5z7PFseFqrvIk7koqZuK_2Q_f0_PCSvNjgys',
       ])
