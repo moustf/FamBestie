fc35d6a57ceac84015c62b07aa8e5c90a7446516
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-04 16:46:31 +0200

    feat: served the hired workers count route in the workers route
    Close: #113

--- server/routes/worker.ts
+++ server/routes/worker.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 
 import { registerWorkerController } from '../controllers/worker/registerWorker';
 import { getWorkersByNameOrSpecialtyController } from '../controllers/worker/getWorkersByNameOrSpecialtyController';
+import { getHiredWorkersCountController } from '../controllers/worker/getHiredWorkersCountController';
 import { userAuth } from '../middlewares/userAuth';
 
 export const workerRouter = Router();
 
 workerRouter.get('', userAuth, getWorkersByNameOrSpecialtyController);
+workerRouter.get('/hired/count', userAuth, getHiredWorkersCountController);
 workerRouter.post('/register', registerWorkerController);
