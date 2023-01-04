ad3cbcc6d6ba90e55cbe58686c47415d7f5056b3
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-04 15:23:44 +0200

    feat: served the route in the worker route
    Close #112

--- server/routes/worker.ts
+++ server/routes/worker.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 
 import { registerWorkerController } from '../controllers/worker/registerWorker';
+import { getWorkersByNameOrSpecialtyController } from '../controllers/worker/getWorkersByNameOrSpecialtyController';
+import { userAuth } from '../middlewares/userAuth';
 
 export const workerRouter = Router();
 
+workerRouter.get('', userAuth, getWorkersByNameOrSpecialtyController);
 workerRouter.post('/register', registerWorkerController);
