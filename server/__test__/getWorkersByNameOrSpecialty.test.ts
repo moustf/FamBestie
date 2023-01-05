38acf6d53926b118a7d50f39bca8dafd7a0483ee
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-05 16:11:13 +0200

    fix: refactored the tests for the route
    Close: #116

--- server/__test__/getWorkersByNameOrSpecialty.test.ts
+++ server/__test__/getWorkersByNameOrSpecialty.test.ts
@@ -61,20 +61,6 @@ describe('Testing getWorkersByNameOrSpecialty route', () => {
       });
   });
 
-  it('In the success path for name querystring, it should return 200 status code and the success message for no left workers.', (done) => {
-    supertest(app)
-      .get('/api/v1/worker?name=qas&offset=100')
-      .set('Cookie', [
-        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEgU2FsZW0iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NzAzMzU0OTZ9.N0be-g3o_BMsqFdi4j5VLpTUpgWKiD9eYT0R2Sm2O3E',
-      ])
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.body.msg).toBe('There is not left workers!');
-        return done();
-      });
-  });
-
   it('In the success path for specialty querystring, it should return 200 status code and the success message.', (done) => {
     supertest(app)
       .get('/api/v1/worker?specialty=driver&offset=1')
@@ -133,14 +119,14 @@ describe('Testing getWorkersByNameOrSpecialty route', () => {
 
   it('In the failure path for specialty querystring, it should return 400 status code and the specialty is invalid.', (done) => {
     supertest(app)
-      .get('/api/v1/worker?name=hi&offset=1')
+      .get('/api/v1/worker?specialty=title&offset=1')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEgU2FsZW0iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NzAzMzU0OTZ9.N0be-g3o_BMsqFdi4j5VLpTUpgWKiD9eYT0R2Sm2O3E',
       ])
-      .expect(404)
+      .expect(400)
       .end((err, res) => {
         if (err) return done(err);
-        expect(res.body.msg).toBe('The name you are searching for does not exist!');
+        expect(res.body.msg).toBe('The specialty you are searching with is invalid!');
         return done();
       });
   });
